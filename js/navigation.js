110c5dbc26d941403984e68f73af765662896dd5
Aya <47668950+AyaPK@users.noreply.github.com>
2021-04-18 12:39:50 +0100

    FIrst commit

---
+++ js/navigation.js
@@ -0,0 +1,36 @@
+$(".navarea .area:nth-child(1) .header span").addClass("navline-selected")
+
+$(".area").click(function () {
+    if ($(this).children(".header").children("span").hasClass("navline-selected")) {
+        var html = $(this).children(".header").children("span");
+        var t = html.html();
+        if (html.html().indexOf("v") != -1) {
+            html.html(t.replace("v", "&gt;"));
+        } else {
+            html.html(t.replace("&gt;", "v"));
+        }
+    } else {
+        $(".navline-selected").removeClass("navline-selected")
+        $(this).children(".header").children("span").addClass("navline-selected")
+        var html = $(this).children(".header").children("span");
+        var t = html.html();
+        if (html.html().indexOf("v") != -1) {
+            html.html(t.replace("v", "&gt;"));
+        } else {
+            html.html(t.replace("&gt;", "v"));
+        }
+    }
+
+    $(this).children(".content").slideToggle(500)
+})
+
+var stringToAdd = "Aya website hahaha...";
+var index = 0;
+function addLetter(){
+    $(".animateTextArea").append(stringToAdd[index]);
+    index++;
+    if(index > stringToAdd.length){
+        clearInterval(running);
+    }
+}
+var running = setInterval(addLetter, 40);
